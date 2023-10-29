7ea60a877e1ca76ae50adce51d776cf61552c081
BHARATHI <dhanabharathimeena@email.com>
2023-10-29 10:19:05 +0530

    day-16

---
+++ DAY-16/script.js
@@ -0,0 +1,72 @@
+setTimeout(() => {
+  const x = document.createElement("div");
+  x.innerText = "Independence Day Countdown Begins!!";
+  const y = document.getElementById("timer");
+  y.appendChild(x);
+  setTimeout(() => {
+    const a = document.createElement("div");
+    a.innerText = "10";
+    const b = document.getElementById("timer");
+    b.appendChild(a);
+    setTimeout(() => {
+      const c = document.createElement("div");
+      c.innerText = "9";
+      const d = document.getElementById("timer");
+      d.appendChild(c);
+      setTimeout(() => {
+        const e = document.createElement("div");
+        e.innerText = "8";
+        const f = document.getElementById("timer");
+        f.appendChild(e);
+        setTimeout(() => {
+          const g = document.createElement("div");
+          g.innerText = "7";
+          const h = document.getElementById("timer");
+          h.appendChild(g);
+          setTimeout(() => {
+            const i = document.createElement("div");
+            i.innerText = "6";
+            const j = document.getElementById("timer");
+            j.appendChild(i);
+            setTimeout(() => {
+              const k = document.createElement("div");
+              k.innerText = "5";
+              const l = document.getElementById("timer");
+              l.appendChild(k);
+              setTimeout(() => {
+                const m = document.createElement("div");
+                m.innerText = "4";
+                const n = document.getElementById("timer");
+                n.appendChild(m);
+                setTimeout(() => {
+                  const o = document.createElement("div");
+                  o.innerText = "3";
+                  const p = document.getElementById("timer");
+                  p.appendChild(o);
+                  setTimeout(() => {
+                    const q = document.createElement("div");
+                    q.innerText = "2";
+                    const r = document.getElementById("timer");
+                    r.appendChild(q);
+                    setTimeout(() => {
+                      const s = document.createElement("div");
+                      s.innerText = "1";
+                      const t = document.getElementById("timer");
+                      t.appendChild(s);
+                      setTimeout(() => {
+                        const u = document.createElement("div");
+                        u.innerText = "Happy Independence Day!!!";
+                        const v = document.getElementById("timer");
+                        v.appendChild(u);
+                      }, 0);
+                    }, 200);
+                  }, 400);
+                }, 600);
+              }, 800);
+            }, 1000);
+          }, 1200);
+        }, 1400);
+      }, 1600);
+    }, 1800);
+  }, 2000);
+}, 2200);
